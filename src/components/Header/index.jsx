b5fb30fb578d728650b5f9ae96fa1e0ba8103e85
Anton Alexandrin <krelesser@yandex.ru>
2021-12-02 18:55:14 +0300

    mock login

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,32 +1,42 @@
 import React from 'react';
-import { AppBar, Button } from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import Login from './components/Login'
+import {AppBar, Button} from '@mui/material';
+import {makeStyles} from '@mui/styles';
+import Login from '../Login'
 import ModalCard from '../ModalCard';
 
 const useStyles = makeStyles((theme) => ({
-  header: {
-    width: '100%',
-    height: 60,
-    display: 'flex',
-    justifyContent: 'space-between',
-    position: 'fixed',
-    top: 0,
-    backgroundColor: 'orange',
-    transition: 'height 0.5s ease-out',
-  },
+    header: {
+        width: '100%',
+        height: 60,
+        display: 'flex',
+        justifyContent: 'space-between',
+        position: 'fixed',
+        top: 0,
+        backgroundColor: 'orange',
+        transition: 'height 0.5s ease-out',
+    },
 }));
 
 const Hero = () => {
-  const classes = useStyles();
-  return (
-    <div className={classes.header} position="static">
-      This is header!
-      <Login open={open} handleClose={handleClose} />
-      <Button>CLICK</Button>
-      <ModalCard />
-    </div>
-);
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <div className={classes.header} position="static">
+            This is header!
+            <Login open={open} handleClose={handleClose}/>
+            <Button>CLICK</Button>
+            <ModalCard/>
+        </div>
+    );
 };
 
 export default Hero;
--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,19 +1,9 @@
 import React, {useState} from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import {makeStyles} from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import {Avatar, Button, Link, Grid, Box, Typography, Container,
+    Dialog, CssBaseline, TextField, FormControlLabel, Checkbox} from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import {makeStyles} from '@mui/styles';
 
-import Dialog from '@material-ui/core/Dialog';
 import {bindActionCreators} from 'redux';
 import * as userActions from '../../actions/user';
 import {connect} from 'react-redux';
