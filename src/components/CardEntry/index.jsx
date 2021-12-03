3d9b428af1c602cc29cf0625e21274459929dc87
Kirill Ateev <kirill.ateev59@gmail.com>
2021-12-03 18:23:20 +0300

    chore

--- src/components/CardEntry/index.jsx
+++ src/components/CardEntry/index.jsx
@@ -1,69 +1,72 @@
 import React from 'react';
-import {makeStyles} from '@mui/styles';
-import {Typography} from '@mui/material';
-import {Link} from 'react-router-dom';
+import { makeStyles } from '@mui/styles';
+import { Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
-    entry: {
-        display: 'flex',
-        justifyContent: 'flex-start',
-        columnGap: '30px',
-        alignItems: 'center',
-    },
-    icon: {
-        width: '75px',
-        height: '75px',
-        borderRadius: '50%',
-        border: '2px solid #000000',
-        flexShrink: 0,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    data: {
-        display: "flex",
-        flexDirection: "column",
-        gap: 6
-    },
-    text: {
-        color: 'gray',
-        display: 'flex',
-        gap: 6,
-        flexDirection: 'column',
-    },
-    items: {
-        display: "flex",
-        color: "black",
-        justifyContent: "space-between",
-        flexWrap: "wrap",
-    },
-    meetingButton: {
-        marginLeft: -14
-    },
-    a: {
-        color: '#303181',
-    },
+  entry: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    columnGap: '30px',
+    alignItems: 'center',
+  },
+  icon: {
+    width: '75px',
+    height: '75px',
+    borderRadius: '50%',
+    border: '2px solid #000000',
+    flexShrink: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  data: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 6,
+  },
+  text: {
+    color: 'gray',
+    display: 'flex',
+    gap: 6,
+    flexDirection: 'column',
+  },
+  items: {
+    display: 'flex',
+    color: 'black',
+    justifyContent: 'space-between',
+    flexWrap: 'wrap',
+  },
+  meetingButton: {
+    marginLeft: -14,
+  },
+  a: {
+    color: '#303181',
+  },
 }));
 
-const CardEntry = ({icon, text, link = '#', children}) => {
-    const classes = useStyles();
+const CardEntry = ({ icon, text, link = '#', children }) => {
+  const classes = useStyles();
 
-    return (
-        <div className={classes.entry}>
-            <div className={classes.icon}>{icon}</div>
-            <div className={classes.data}>
-                <div className={classes.text}>
-                    <div className={classes.items}>{
-                        children ? text.map(item => <div>{item}</div>)
-                    : text}</div>
-                </div>
-                <div>
-                    {children ? <div className={classes.meetingButton}>{children}</div>
-                    : <Link to={link}>Перейти →</Link>}
-                </div>
-            </div>
+  return (
+    <div className={classes.entry}>
+      <div className={classes.icon}>{icon}</div>
+      <div className={classes.data}>
+        <div className={classes.text}>
+          <div className={classes.items}>
+            {children ? text.map((item) => <div>{item}</div>) : text}
+          </div>
         </div>
-    );
+        <div>
+          {children ? (
+            <div className={classes.meetingButton}>{children}</div>
+          ) : (
+            <Link to={link}>Перейти →</Link>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CardEntry;
--- src/components/Courses/index.jsx
+++ src/components/Courses/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { courseCardData } from '../../constants/constantValues';
 import CourseCard from './CourseCard';
-import {makeStyles} from '@mui/styles';
+import { makeStyles } from '@mui/styles';
+import { Typography } from '@mui/material';
 
 const useStyles = makeStyles((theme) => ({
   coursesContainer: {
-      display: 'flex',
-      gap: 20,
+    display: 'flex',
+    gap: 20,
     paddingTop: 'min(140px, 8%)',
     paddingLeft: 'min(140px, 8%)',
     paddingRight: 'min(140px, 8%)',
@@ -14,7 +15,7 @@ const useStyles = makeStyles((theme) => ({
   title: {
     fontWeight: 'bold !important',
     marginBottom: '30px !important',
-  }
+  },
 }));
 
 const Courses = () => {
@@ -22,7 +23,9 @@ const Courses = () => {
 
   return (
     <div className={classes.coursesContainer}>
-        <Typography className={classes.title} variant="h4">Вводные курсы</Typography>
+      <Typography className={classes.title} variant="h4">
+        Вводные курсы
+      </Typography>
       {courseCardData.map((card) => (
         <CourseCard text={card.text} link={card.link} />
       ))}
